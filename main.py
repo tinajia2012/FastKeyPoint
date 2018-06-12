57e2cd49d35d0285bb649dfcf1073b65b35042aa
tinajia2012 <296423420@qq.com>
2018-06-12 19:56:58 +0800

    add new file:main.py
    add by zjx in 2018.06.12

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 import os
 
+def get_data(data_root, is_train=True, is_val=False):
+  dataset = [file for file in os.listdir(data_root)]
+    
+
 if __name__ == '__main__':
   data_root = sys.argv[1]
-  train_data, val_data = get_data(data_root)
+  train_data, val_data = get_data(data_root, is_train, is_val)
   net = get_net()
   train(train_data, val_data, net)
